3e888268de95c89e8ec399098c654c1b7ef72244
fp <huang.fp@gmail.com>
2021-12-31 20:32:41 -0600

    add collections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Homepage} from './pages/homepage/homepage.component'
+import { Homepage } from './pages/homepage/homepage.component';
+import { Route, Routes } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/shop' element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
